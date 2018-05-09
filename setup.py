2bce943197fad61ff8b6211ccaee691fd3cf0d88
Matthias Endler <matthias-endler@gmx.net>
2018-05-09 23:52:23 +0200

    Extend setup

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
+import sys
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
 
-#
+try:
+    from setuptools_rust import RustExtension
+except ImportError:
+    import subprocess
+    errno = subprocess.call(
+        [sys.executable, '-m', 'pip', 'install', 'setuptools-rust'])
+    if errno:
+        print("Please install setuptools-rust package")
+        raise SystemExit(errno)
+    else:
+        from setuptools_rust import RustExtension
+
+setup_requires = ['setuptools-rust>=0.6.0']
+install_requires = []
+
 setup(name='hyperjson',
       version='0.1',
-      rust_extensions=[
-          RustExtension('hyperjson._hyperjson',
-                        'Cargo.toml', binding=Binding.PyO3)],
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Rust',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+      ],
       packages=['hyperjson'],
-      # rust extensions are not zip safe, just like C-extensions.
+      include_package_data=True,
+      rust_extensions=[
+          RustExtension('hyperjson._hyperjson', 'Cargo.toml')],
       zip_safe=False)
