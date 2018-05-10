68e4f0a37a1b32ff0b97772f54f0f585dce0b788
Matthias Endler <matthias-endler@gmx.net>
2018-05-10 19:56:51 +0200

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -103,7 +103,7 @@ fn init(py: Python, m: &PyModule) -> PyResult<()> {
         separators: Option<PyObject>,
         default: Option<PyObject>,
         sort_keys: Option<PyObject>,
-        kwargs: Option<&PyDict>
+        kwargs: Option<&PyDict>,
     ) -> PyResult<PyObject> {
         let v = to_json(py, &obj)?;
         let s: Result<String, HyperJsonError> =
@@ -116,14 +116,31 @@ fn init(py: Python, m: &PyModule) -> PyResult<()> {
         py: Python,
         obj: PyObject,
         fp: PyObject,
-         skipkeys: Option<PyObject>,
-                ensure_ascii: Option<PyObject>,
-                check_circular: Option<PyObject>, allow_nan: Option<PyObject>,
-                cls: Option<PyObject>, indent: Option<PyObject>,
-                separators: Option<PyObject>, default: Option<PyObject>,
-                sort_keys: Option<PyObject>, kwargs: Option<&PyDict>
+        skipkeys: Option<PyObject>,
+        ensure_ascii: Option<PyObject>,
+        check_circular: Option<PyObject>,
+        allow_nan: Option<PyObject>,
+        cls: Option<PyObject>,
+        indent: Option<PyObject>,
+        separators: Option<PyObject>,
+        default: Option<PyObject>,
+        sort_keys: Option<PyObject>,
+        kwargs: Option<&PyDict>,
     ) -> PyResult<PyObject> {
-        let s = dumps_fn(py, obj, None, None, None, None,None,None,None,None,None,None)?;
+        let s = dumps_fn(
+            py,
+            obj,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )?;
         let fp_ref: &PyObjectRef = fp.as_ref(py);
         fp_ref.call_method1("write", (s,))?;
         // TODO: Will this always return None?
