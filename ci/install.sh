91defa5406cf28b73a1d057060e79f3478f7aa66
Matthias Endler <matthias-endler@gmx.net>
2018-08-05 21:03:23 +0200

    Add missing deps

--- ci/install.sh
+++ ci/install.sh
@@ -9,6 +9,7 @@ main() {
     which rustc
     rustc --version
     pip install pipenv
+    apt-get update && apt-get install libdbus-1-3
     cargo install pyo3-pack
     make install
     pipenv graph
