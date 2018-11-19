6c7ac943df838f0ecf2126f024e4faca3acbf416
Matthias Endler <matthias-endler@gmx.net>
2018-11-20 00:22:24 +0100

    counting is hard

--- profiling/src/profiles/booleans.rs
+++ profiling/src/profiles/booleans.rs
@@ -4,7 +4,7 @@ pub fn exec(iterations: u64) {
     let gil = Python::acquire_gil();
     let py = gil.python();
 
-    let booleans = format!("[{} \"true\"]", "\"true\",".repeat(256));
+    let booleans = format!("[{} \"true\"]", "\"true\",".repeat(255));
     let obj = booleans.to_object(py);
 
     for _ in 0..iterations {
